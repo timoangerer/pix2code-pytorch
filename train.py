f8614e05fa9267a20475389a9fdf467d8fe1f165
timoangerer <timo.angerer@code.berlin>
2021-04-12 15:59:58 +0200

    Implement training and models

---
+++ train.py
@@ -0,0 +1,119 @@
+import argparse
+from pathlib import Path
+from vocab import Vocab
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from dataset import Pix2CodeDataset
+from utils import collate_fn, save_model
+from models import Encoder, Decoder
+import math
+
+parser = argparse.ArgumentParser(description='Train the model')
+
+parser.add_argument("--data_path", type=str,
+                    default=Path("data", "screenshot-description-pairs"), help="Datapath")
+parser.add_argument("--vocab_file_path", type=str,
+                    default=Path("data", "vocab.txt"), help="Path to the vocab file")
+parser.add_argument("--cuda", action='store_true',
+                    default=True, help="Use cuda or not")
+parser.add_argument("--img_crop_size", type=int, default=224)
+parser.add_argument("--split", type=str, default="train")
+parser.add_argument("--save_after_epochs", type=int, default=1,
+                    help="Save model checkpoint after n epochs")
+parser.add_argument("--models_dir", type=str, default=Path("models"),
+                    help="The dir where the trained models saved")
+parser.add_argument("--batch_size", type=int, default=4)
+parser.add_argument("--epochs", type=int, default=15)
+parser.add_argument("--lr", type=float, default=1e-3,
+                    help="Learning Rate")
+parser.add_argument("--print_freq", type=int, default=1,
+                    help="The frequency to print message (epochs)")
+parser.add_argument("--seed", type=int, default=2020,
+                    help="The random seed for reproducing ")
+
+args = parser.parse_args()
+print("Training args:", args)
+
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed(args.seed)
+
+# Load the vocab file
+vocab = Vocab(args.vocab_file_path)
+assert len(vocab) > 0
+
+use_cuda = True if args.cuda and torch.cuda.is_available() else False
+assert use_cuda  # Trust me, you don't want to train this model on a cpu.
+device = torch.device("cuda" if use_cuda else "cpu")
+
+
+# Image transformation function for resnet152: https://pytorch.org/docs/stable/torchvision/models.html
+transform_imgs = transforms.Compose([transforms.Resize((args.img_crop_size, args.img_crop_size)),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                          std=[0.229, 0.224, 0.225])])
+
+# Creating the data loader
+print("Creating the data loader...")
+train_loader = DataLoader(
+    Pix2CodeDataset(args.data_path, args.split,
+                    vocab, transform=transform_imgs),
+    batch_size=args.batch_size,
+    collate_fn=collate_fn,
+    pin_memory=True if use_cuda else False,
+    num_workers=4,
+    drop_last=True)
+
+# Creating the models
+embed_size = 256
+hidden_size = 512
+num_layers = 1
+lr = args.lr
+
+encoder = Encoder(embed_size)
+decoder = Decoder(embed_size, hidden_size, len(vocab), num_layers)
+
+encoder = encoder.to(device)
+decoder = decoder.to(device)
+
+# Define optimizer and loss function
+criterion = torch.nn.CrossEntropyLoss()
+params = list(decoder.parameters()) + list(encoder.linear.parameters()
+                                           ) + list(encoder.BatchNorm.parameters())
+optimizer = torch.optim.Adam(params, lr=lr)
+
+# Training the model
+for epoch in range(args.epochs):
+    for i, (images, captions, lengths) in enumerate(train_loader):
+        images = images.to(device)
+        captions = captions.to(device)
+
+        targets = torch.nn.utils.rnn.pack_padded_sequence(
+            input=captions, lengths=lengths, batch_first=True)[0]
+
+        encoder.zero_grad()
+        decoder.zero_grad()
+
+        features = encoder(images)
+        output = decoder(features, captions, lengths)
+        loss = criterion(output, targets)
+
+        loss.backward()
+        optimizer.step()
+
+        loss = loss.item()
+
+        if epoch % args.print_freq == 0 and i == 0:
+            print(
+                f'Epoch : {epoch} || Loss : {loss} || Perplexity : {math.exp(loss)}')
+
+        if epoch % args.save_after_epochs == 0 and i % len(train_loader) == 0:
+            print("Saving Models")
+            save_model(args.models_dir, encoder, decoder,
+                       optimizer, epoch, loss, args.batch_size, vocab)
+
+print("Done Training!")
+
+print("Saving final model")
+save_model(args.models_dir, encoder, decoder,
+           optimizer, epoch, loss, args.batch_size, vocab)
---
+++ utils.py
@@ -0,0 +1,70 @@
+import time
+import torch
+from pathlib import Path
+
+# Taken from: https://github.com/yunjey/pytorch-tutorial/blob/0500d3df5a2a8080ccfccbc00aca0eacc21818db/tutorials/03-advanced/image_captioning/data_loader.py#L56
+
+
+def collate_fn(data):
+    """Creates mini-batch tensors from the list of tuples (image, caption).
+
+    We should build custom collate_fn rather than using default collate_fn, 
+    because merging caption (including padding) is not supported in default.
+    Args:
+        data: list of tuple (image, caption). 
+            - image: torch tensor of shape (3, 256, 256).
+            - caption: torch tensor of shape (?); variable length.
+    Returns:
+        images: torch tensor of shape (batch_size, 3, 256, 256).
+        targets: torch tensor of shape (batch_size, padded_length).
+        lengths: list; valid length for each padded caption.
+    """
+    # Sort a data list by caption length (descending order).
+    data.sort(key=lambda x: len(x[1]), reverse=True)
+    images, captions = zip(*data)
+
+    # Merge images (from tuple of 3D tensor to 4D tensor).
+    images = torch.stack(images, 0)
+
+    # Merge captions (from tuple of 1D tensor to 2D tensor).
+    lengths = [len(cap) for cap in captions]
+    targets = torch.zeros(len(captions), max(lengths)).long()
+    for i, cap in enumerate(captions):
+        end = lengths[i]
+        targets[i, :end] = cap[:end]
+    return images, targets, lengths
+
+
+def save_model(models_folder_path, encoder, decoder, optimizer, epoch, loss, batch_size, vocab):
+    MODELS_FOLDER = Path(models_folder_path)
+
+    # Create the models folder if it's not already there
+    MODELS_FOLDER.mkdir(parents=True, exist_ok=True)
+
+    MODEL_PATH = MODELS_FOLDER / (model_name_formated("e-d-model",
+                                  {"epoch": epoch, "loss": loss, "batch": batch_size}) + ".pth")
+
+    torch.save({'epoch': epoch,
+                'encoder_model_state_dict': encoder.state_dict(),
+                'decoder_model_state_dict': decoder.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': loss,
+                'vocab': vocab
+                }, MODEL_PATH)
+
+# Util for better model names when saving
+
+
+def model_name_formated(model_name, stats_dict, delimiter="--"):
+    current_time = time.strftime("%d-%m-%H-%M")
+    stats_dict["time"] = current_time
+
+    file_name = model_name
+
+    for key, value in stats_dict.items():
+        if isinstance(value, float):
+            value = f'{value:.4f}'
+
+        file_name = file_name + delimiter + str(key) + "-" + str(value)
+
+    return file_name
